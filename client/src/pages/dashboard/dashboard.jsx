994c316aa68471806e0745059754dde78aa006cc
ArthurSoares22 <arthur.pimentel02@hotmail.com>
2026-06-05 14:25:09 -0300

    feat:dd login poage and dashboard with task list

--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -1,8 +1,30 @@
-function dashboard(){
-  return (<div>
-    <h1>dashboard</h1>
-   </div>
-  )
+import { useState } from "react";
+import { useEffect } from "react";
+import api from '../../services/api'
+
+function  Dashboard(){
+  const [tasks, setTask] = useState([])
+
+  useEffect(() =>{
+    async function fetchTasks() {
+       const resposta = await api.get('/tasks')
+    console.log(resposta.data)
+    setTask(resposta.data)
+    }
+   fetchTasks()
+  }, [])
+
+
+  return (
+  <div>
+    <h1>Dashboard</h1>
+    {tasks.map((task) => (
+      <div key={task.id}>
+        <p>{task.title}</p>
+      </div>
+    ))}
+  </div>
+)
 }
 
-export default dashboard;
+export default Dashboard;
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,8 +1,48 @@
-function login(){
-  return (<div>
-    <h1>Login</h1>
-   </div>
-  )
+import { useState } from "react";
+import api from "../../services/api";
+import {useNavigate} from 'react-router-dom'
+//funcão de criar login
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+
+
+  async function handleLogin(){
+  try {
+    const resposta = await api.post('/auth/login', {email, password})
+    const token = resposta.data.token
+    localStorage.setItem('token', token)
+    //para redirecionar a pagina
+navigate ('/dashboard')
+  } catch (error) {
+    console.log("Erro")
+  }
 }
 
-export default login;
+  return (
+  <div>
+    <input
+      type="text"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+    />
+
+    <input 
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+ />
+
+    <button onClick={handleLogin}>Entrar</button>
+  </div>
+)
+
+
+}
+
+
+
+
+export default Login;
